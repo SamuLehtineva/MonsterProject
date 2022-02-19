7c15b17107657c7c61fdbc3b8a7d84a684192964
s <samu.lehtineva@gmail.com>
2022-02-19 16:03:15 +0200

    Added look z offset to FollowTransform

---
+++ MonsterProject/Assets/Scripts/Quest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    public class Quest : MonoBehaviour
+    {
+        
+        void Start()
+        {
+        
+        }
+
+        
+        void Update()
+        {
+        
+        }
+    }
+}
