6be31b14b10757f8026c840e7a9ab372cab07e1e
Amanda <amanda.vanhatalo@tuni.fi>
2022-03-10 14:46:56 +0200

    Implemented MiniGameInterface

---
+++ MonsterProject/Assets/Scripts/Minigames/MiniGameInterface.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    public interface MiniGameInterface
+    {
+        void StartMiniGame();
+
+        void WinMiniGame();
+
+        void LoseMiniGame();
+    }
+}
