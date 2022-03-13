5480d44b332acc661ba85abb23dbf63ed47cd3ed
s <samu.lehtineva@gmail.com>
2022-03-13 18:40:49 +0200

    Small fixes to things found in build

--- MonsterProject/Assets/Scripts/Util/CharMover.cs
+++ MonsterProject/Assets/Scripts/Util/CharMover.cs
@@ -50,10 +50,9 @@ namespace GA.MonsterProject
                 }
 
                 TryPet();
-                    
             }
             
-            else if (Input.GetKeyDown("escape"))
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
                 SceneChanger.LoadLevel("Menu");
             }
