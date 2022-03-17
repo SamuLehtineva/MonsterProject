01bb7c072df72b07f5b091ec199e7731054b806b
Amanda <amanda.vanhatalo@tuni.fi>
2022-03-17 15:55:30 +0200

    SimonSays Update - not ready yet

--- MonsterProject/Assets/Scripts/Minigames/SimonMiniGame/SimonSays.cs
+++ MonsterProject/Assets/Scripts/Minigames/SimonMiniGame/SimonSays.cs
@@ -6,18 +6,76 @@ namespace GA.MonsterProject
 {
     public class SimonSays : MonoBehaviour
     {
-        public Animator anim;
+        [Header("Monster")]
+        public GameObject MonsterLeft;
+        public GameObject MonsterUp;
+        public GameObject MonsterRight;
+
+        [Header("Player")]
+        public GameObject PlayerLeft;
+        public GameObject PlayerUp;
+        public GameObject PlayerRight;
+
+        public bool rightChoice = false;
+
+        public int RandomNumber;
+        public int Points = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ActivateButton();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
 
         void OnMouseDown()
         {
-            Action();
+            //Debug.Log(gameObject);
+            if(gameObject.tag == "PlayerLeft" && MonsterLeft.activeSelf)
+            {
+                MonsterLeft.SetActive(false);
+                Points = +1;
+            }
+            if (gameObject.tag == "PlayerUp" && MonsterUp.activeSelf)
+            {
+                MonsterUp.SetActive(false);
+                Points = +1;
+            }
+            if (gameObject.tag == "PlayerRight" && MonsterRight.activeSelf)
+            {
+                MonsterRight.SetActive(false);
+                Points = +1;
+            }
+            Debug.Log(Points);
         }
-        void Action()
+
+        void ActivateButton()
         {
-            anim.enabled = true;
-            anim.SetTrigger("pop");
+            RandomNum();
+
+            if (RandomNumber == 0) {
+                MonsterLeft.SetActive(true);
+            } else if (RandomNumber == 1)
+            {
+                MonsterUp.SetActive(true);
+            } else //(RandomNumber == 2)
+            {
+                MonsterRight.SetActive(true);
+            }
         }
+
+        void RandomNum()
+        {
+            RandomNumber = Random.Range(0, 3);
+            Debug.Log(RandomNumber);
+        }
+
+        
 
     }
-
 }
