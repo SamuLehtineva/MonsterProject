5e639a33d9d705288283f70e98a2fda3708efe4a
s <samu.lehtineva@gmail.com>
2022-03-20 16:51:25 +0200

    Updated health bars / other stuff for battle

--- MonsterProject/Assets/Scripts/Minigames/FightingMinigame/Minigame1.cs
+++ MonsterProject/Assets/Scripts/Minigames/FightingMinigame/Minigame1.cs
@@ -17,10 +17,10 @@ namespace GA.MonsterProject
         public bool m_bIsMyTurn;
 
         [SerializeField]
-        TMP_Text m_txtEnemyHealthText;
+        UIBar m_gcEnemyBar;
 
         [SerializeField]
-        TMP_Text m_txtPetHealthText;
+        UIBar m_gcPetBar;
 
         [SerializeField]
         TMP_Text m_txtTurnText;
@@ -32,8 +32,8 @@ namespace GA.MonsterProject
 
         void Update()
         {
-            m_txtEnemyHealthText.text = "Health: " + m_iEnemyHealth;
-            m_txtPetHealthText.text = "Health: " + m_iPetHealth;
+            m_gcEnemyBar.SetWidth(m_iEnemyHealth / 100f);
+            m_gcPetBar.SetWidth(m_iPetHealth / 100f);
 
             if (Input.GetButtonDown("Fire2"))
             {
@@ -61,24 +61,17 @@ namespace GA.MonsterProject
                 if (m_bIsMyTurn)
                 {
                     m_iEnemyHealth -= 50;
-                    m_txtEnemyHealthText.color = Color.red;
                 }
                 else
                 {
                     m_iPetHealth -= 25;
-                    m_txtPetHealthText.color = Color.yellow;
                 }
             } 
             else
             {
-                
-                if (m_bIsMyTurn)
+                if (!m_bIsMyTurn)
                 {
-                    m_txtEnemyHealthText.color = Color.yellow;
-                }
-                else{
                     m_iPetHealth -= 50;
-                    m_txtPetHealthText.color = Color.red;
                 }
             }
             
@@ -107,8 +100,6 @@ namespace GA.MonsterProject
             yield return new WaitForSeconds(1);
             m_gcMoveBetween.enabled = true;
             m_gcMoveBetween.Reset();
-            m_txtEnemyHealthText.color = Color.white;
-            m_txtPetHealthText.color = Color.white;
             m_bIsMyTurn = !m_bIsMyTurn;
         }
 
