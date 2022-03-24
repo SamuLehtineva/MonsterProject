0195fdebfdc513993b5612e9ba117944f41a3c31
s <samu.lehtineva@gmail.com>
2022-03-24 16:40:37 +0200

    Started work on quest log

---
+++ MonsterProject/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    [ExecuteInEditMode]
+    public class QuestManager : MonoBehaviour
+    {
+        QuestInfo[] m_aQuests;
+        // Start is called before the first frame update
+        public void OnValidate()
+        {
+            m_aQuests = GetComponentsInChildren<QuestInfo>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ MonsterProject/Assets/Scripts/Util/QuestInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    public class QuestInfo : MonoBehaviour
+    {
+        public string m_sName;
+        public Types.EStatus m_iStatus = Types.EStatus._Active;
+        public string m_sDescription;
+    }
+}
