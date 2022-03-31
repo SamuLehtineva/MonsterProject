46f92c50bd6926d46f82c0f710d177b7cfd262fd
s <samu.lehtineva@gmail.com>
2022-03-31 13:17:23 +0300

    Removed unused code

--- MonsterProject/Assets/Scripts/NPC/Npc_ChickenMan.cs
+++ MonsterProject/Assets/Scripts/NPC/Npc_ChickenMan.cs
@@ -11,7 +11,6 @@ namespace GA.MonsterProject
 
         [SerializeField]
         Npc_Trigger m_gcTrigger;
-        static bool m_bTriggered = false;
 
         void Update()
         {
@@ -58,18 +57,6 @@ namespace GA.MonsterProject
 
                     break;
             }
-            /*if (UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("fetch_helga").m_iStatus == Types.EStatus._Completed)
-            {
-                m_gcTalk.m_sFileName = "fetch_helga_complete";
-                m_gcTalk.m_bRewardTalk = true;
-                m_gcTalk.m_bKillAfterUse = true;
-            }
-
-            if (!m_gcTrigger.gameObject.activeInHierarchy && !m_bTriggered)
-            {
-                m_gcTalk.gameObject.SetActive(true);
-                m_bTriggered = true;
-            }*/
         }
     }
 }
