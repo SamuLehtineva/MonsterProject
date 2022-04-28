e741d2be714454d60b1b53d496c49d273bab8bec
Amanda <amanda.vanhatalo@tuni.fi>
2022-04-28 12:57:37 +0300

    Added Randvi Quest

---
+++ MonsterProject/Assets/Scripts/NPC/Npc_Randvi.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    public class Npc_Randvi : MonoBehaviour
+    {
+        [SerializeField]
+        Npc_Talk m_gcTalk;
+
+        void Update()
+        {
+            Types.EStatus iEStatus = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("deliver_package").m_iStatus;
+            switch (iEStatus)
+            {
+                case Types.EStatus._Inactive:
+                    m_gcTalk.m_sFileName = "randvi_inactive";
+
+                    break;
+
+                case Types.EStatus._Active:
+                    m_gcTalk.m_sFileName = "randvi_active";
+
+                    break;
+
+                case Types.EStatus._Completed:
+                    m_gcTalk.m_sFileName = "randvi_completed";
+                    m_gcTalk.m_qRewardA = new QuestReward(0, 10, 0);
+                    m_gcTalk.m_eQuestStatusA = Types.EStatus._Done;
+                    m_gcTalk.m_bRewardTalk = true;
+
+                    break;
+
+                case Types.EStatus._Done:
+                    m_gcTalk.m_sFileName = "randvi_done";
+                    m_gcTalk.m_bRewardTalk = false;
+                    m_gcTalk.ShowIcon(false);
+
+                    break;
+
+                case Types.EStatus._Failed:
+                    m_gcTalk.m_sFileName = "randvi_failed";
+                    m_gcTalk.ShowIcon(false);
+
+                    break;
+            }
+        }
+    }
+}
