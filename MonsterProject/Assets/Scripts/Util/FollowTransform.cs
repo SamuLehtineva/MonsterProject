8370658992ef4c23edd948c6bd91a3a8340bbe00
s <samu.lehtineva@gmail.com>
2022-05-18 13:24:38 +0300

    Added commenting to follow transform

--- MonsterProject/Assets/Scripts/Util/FollowTransform.cs
+++ MonsterProject/Assets/Scripts/Util/FollowTransform.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 namespace GA.MonsterProject
 {
+    // This script causes the attached gameobject to follow the specified transform
     public class FollowTransform : MonoBehaviour
     {
+        // The transform of the target to be followed
         public Transform m_tTarget;
 
+        // These offsets define attached objects position in relation to the target
+        // Setting all to 0 sets position to targets position
         public float m_fXOffset;
         public float m_fYOffset;
         public float m_fZOffset;
 
+        // These offsets define where attached object is looking in relation to the target
         public float m_fXLookOffset = 0;
         public float m_fYLookOffset = 0;
         public float m_fZLookOffset;
@@ -25,7 +30,6 @@ namespace GA.MonsterProject
             vWantedPos.x += m_fXOffset;
             vWantedPos.z += m_fZOffset;
 
-            //transform.position = Vector3.Lerp(vCurrentPos, vWantedPos, 25.0f * Time.deltaTime);
             transform.position = vWantedPos;
             Vector3 vTarget = m_tTarget.transform.position;
             vTarget += new Vector3(m_fXLookOffset, m_fYLookOffset, m_fZLookOffset);
