856d75f7d12267f7c4d61a4f76b61a5b3d818df1
Amanda <amanda.vanhatalo@tuni.fi>
2022-05-18 16:48:51 +0300

    Commented the code

--- MonsterProject/Assets/Scripts/Minigames/RunningMinigame/RunMiniCamera.cs
+++ MonsterProject/Assets/Scripts/Minigames/RunningMinigame/RunMiniCamera.cs
@@ -7,6 +7,7 @@ namespace GA.MonsterProject
     public class RunMiniCamera : MonoBehaviour
     {
         public Transform player;
+        //Offsetting the camera lower than the basic settings so the game looks more like a 2d game
         public float m_fXOffset = 6f;
         public float m_fYOffset = -4f;
         public float m_fZOffset = -10f;
