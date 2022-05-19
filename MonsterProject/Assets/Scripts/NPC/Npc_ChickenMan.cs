77039fb035cf7e11bd68cf1caeb1c8d8631751c5
s <samu.lehtineva@gmail.com>
2022-05-19 16:59:17 +0300

    Started work on 2 quests

--- MonsterProject/Assets/Scripts/NPC/Npc_ChickenMan.cs
+++ MonsterProject/Assets/Scripts/NPC/Npc_ChickenMan.cs
@@ -14,58 +14,82 @@ namespace GA.MonsterProject
 
         void Update()
         {
-            Types.EStatus iEStatus = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("fetch_helga").m_iStatus;
-            switch (iEStatus)
+            Types.EStatus iEStatus = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("ate_chicken").m_iStatus;
+            if (iEStatus == Types.EStatus._Disabled)
             {
-                case Types.EStatus._Inactive:
-                    m_gcTrigger.gameObject.SetActive(true);
-                    m_gcTrigger.m_sFileName = "fetch_helga/inactive";
-                    m_gcTrigger.m_bRewardTalk = false;
+                iEStatus = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("fetch_helga").m_iStatus;
+                switch (iEStatus)
+                {
+                    case Types.EStatus._Inactive:
+                        m_gcTrigger.gameObject.SetActive(true);
+                        m_gcTrigger.m_sFileName = "fetch_helga/inactive";
+                        m_gcTrigger.m_bRewardTalk = false;
 
-                    m_gcTalk.gameObject.SetActive(false);
+                        m_gcTalk.gameObject.SetActive(false);
 
-                    break;
+                        break;
 
-                case Types.EStatus._Active:
-                    m_gcTrigger.gameObject.SetActive(false);
+                    case Types.EStatus._Active:
+                        m_gcTrigger.gameObject.SetActive(false);
 
-                    m_gcTalk.gameObject.SetActive(true);
-                    m_gcTalk.m_sFileName = "fetch_helga/active";
-                    m_gcTalk.m_bRewardTalk = false;
-                    m_gcTalk.ShowIcon(false);
+                        m_gcTalk.gameObject.SetActive(true);
+                        m_gcTalk.m_sFileName = "fetch_helga/active";
+                        m_gcTalk.m_bRewardTalk = false;
+                        m_gcTalk.ShowIcon(false);
 
-                    break;
-                
-                case Types.EStatus._Completed:
-                    m_gcTrigger.gameObject.SetActive(false);
+                        break;
+                    
+                    case Types.EStatus._Completed:
+                        m_gcTrigger.gameObject.SetActive(false);
 
-                    m_gcTalk.gameObject.SetActive(true);
-                    m_gcTalk.m_sFileName = "fetch_helga/complete";
-                    m_gcTalk.m_bRewardTalk = true;
-                    m_gcTalk.ShowIcon(true);
+                        m_gcTalk.gameObject.SetActive(true);
+                        m_gcTalk.m_sFileName = "fetch_helga/complete";
+                        m_gcTalk.m_bRewardTalk = true;
+                        m_gcTalk.ShowIcon(true);
 
-                    break;
-                
-                case Types.EStatus._Done:
-                    m_gcTrigger.gameObject.SetActive(false);
+                        break;
+                    
+                    case Types.EStatus._Done:
+                        m_gcTrigger.gameObject.SetActive(false);
 
-                    m_gcTalk.gameObject.SetActive(true);
-                    m_gcTalk.m_sFileName = "fetch_helga/done";
-                    m_gcTalk.m_bRewardTalk = false;
-                    m_gcTalk.ShowIcon(false);
+                        m_gcTalk.gameObject.SetActive(true);
+                        m_gcTalk.m_sFileName = "fetch_helga/done";
+                        m_gcTalk.m_bRewardTalk = false;
+                        m_gcTalk.ShowIcon(false);
 
-                    break;
+                        break;
 
-                case Types.EStatus._Failed:
-                    m_gcTrigger.gameObject.SetActive(false);
+                    case Types.EStatus._Failed:
+                        m_gcTrigger.gameObject.SetActive(false);
 
-                    m_gcTalk.gameObject.SetActive(true);
-                    m_gcTalk.m_sFileName = "fetch_helga/failed";
-                    m_gcTalk.m_bRewardTalk = false;
-                    m_gcTalk.ShowIcon(false);
+                        m_gcTalk.gameObject.SetActive(true);
+                        m_gcTalk.m_sFileName = "fetch_helga/failed";
+                        m_gcTalk.m_bRewardTalk = false;
+                        m_gcTalk.ShowIcon(false);
 
-                    break;
+                        break;
+                }
             }
+            else
+            {
+                switch (iEStatus)
+                {
+                    case Types.EStatus._Inactive:
+                        m_gcTrigger.gameObject.SetActive(true);
+                        m_gcTrigger.m_sFileName = "Ate_chicken_inactive";
+                        m_gcTrigger.m_qRewardA = new QuestReward(-20, 5, -5);
+                        m_gcTrigger.m_sQuestNameA = "ate_chicken";
+                        m_gcTrigger.m_eQuestStatusA = Types.EStatus._Done;
+                        m_gcTrigger.m_qRewardB = new QuestReward(0, 0, 0);
+                        m_gcTrigger.m_sQuestNameB = "ate_chicken";
+                        m_gcTrigger.m_eQuestStatusB = Types.EStatus._Failed;
+
+                        m_gcTalk.gameObject.SetActive(false);
+
+                        break;
+                }
+            }
+            
         }
     }
 }
---
+++ MonsterProject/Assets/Scripts/NPC/Npc_Jens.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GA.MonsterProject
+{
+    public class Npc_Jens : MonoBehaviour
+    {
+        [SerializeField]
+        Npc_Talk m_gcTalk;
+
+        void Update()
+        {
+            Types.EStatus iEStatus = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName("move_rock").m_iStatus;
+            switch (iEStatus)
+            {
+                case Types.EStatus._Disabled:
+                    m_gcTalk.gameObject.SetActive(false);
+                    m_gcTalk.ShowIcon(false);
+                    break;
+
+                case Types.EStatus._Inactive:
+                    m_gcTalk.m_sFileName = "move_rock/inactive";
+                    m_gcTalk.gameObject.SetActive(true);
+
+                    break;
+
+                case Types.EStatus._Active:
+                    m_gcTalk.m_sFileName = "move_rock/active";
+                    m_gcTalk.ShowIcon(false);
+
+                    break;
+
+                case Types.EStatus._Completed:
+                    m_gcTalk.m_sFileName = "move_rock/completed";
+                    m_gcTalk.m_qRewardA = new QuestReward(10, 10, -10);
+                    m_gcTalk.m_eQuestStatusA = Types.EStatus._Done;
+                    m_gcTalk.m_bRewardTalk = true;
+
+                    break;
+
+                case Types.EStatus._Done:
+                    m_gcTalk.m_sFileName = "move_rock/done";
+                    m_gcTalk.m_bRewardTalk = false;
+                    m_gcTalk.ShowIcon(false);
+
+                    break;
+
+                case Types.EStatus._Failed:
+                    m_gcTalk.m_sFileName = "move_rock/failed";
+                    m_gcTalk.ShowIcon(false);
+
+                    break;
+            }
+        }
+    }
+}
