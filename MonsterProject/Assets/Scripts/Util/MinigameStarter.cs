5a8ad74feb146e53974aa4bc8ddef76c4cf2720d
s <samu.lehtineva@gmail.com>
2022-05-27 12:24:13 +0300

    Battle minigame transition

--- MonsterProject/Assets/Scripts/Util/MinigameStarter.cs
+++ MonsterProject/Assets/Scripts/Util/MinigameStarter.cs
@@ -30,6 +30,7 @@ namespace GA.MonsterProject
         public string m_SceneName;
         public string m_sQuestName;
         public string m_sSpawnPoint;
+        public SceneChanger m_gcChanger;
 
         void Start()
         {
@@ -74,7 +75,7 @@ namespace GA.MonsterProject
         {
             GameManager.m_sDestination = m_sSpawnPoint;
             GameManager.m_qMinigameQuest = UIManager.s_UIManager.m_gcQuestManager.GetQuestByName(m_sQuestName);
-            SceneChanger.LoadLevel(m_SceneName);
+            m_gcChanger.BattleStart(m_SceneName);
         }
     }
 }
